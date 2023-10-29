b2e5c24e15789a810b375511bb801c3e64afff1a
samuelfreire03 <s.freire@uniandes.edu.co>
2023-10-28 23:33:33 -0500

    cambios en web

--- Proyecto1/Entrega2/Entrega2/PaginaWebBi/PaginaWeb.py
+++ Proyecto1/Entrega2/Entrega2/PaginaWebBi/PaginaWeb.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import requests
 import pandas as pd
-import io
+import os
 
 app = Flask(__name__)
 
@@ -21,7 +21,25 @@ def upload_file():
 
         response = requests.post('http://127.0.0.1:8000/predict', json=df.to_dict(orient='records'))
 
+        df1 = pd.DataFrame(response.json())
+
+        df1 = df1.drop(['Textos_Tokenizados'], axis=1)
+
+        data_dir = os.path.join(os.getcwd(), './Proyecto1/Entrega2/Entrega2/PaginaWebBi/data')
+
+        nombre_archivo = 'resultado.csv'
+
+        resultado_path = os.path.join(data_dir, nombre_archivo)
+
+        df1.to_csv(resultado_path, index=False)
+
+        send_file(resultado_path, as_attachment=True)
+
         return render_template('results.html', predictions=response.json())
+    
+@app.route('/download_resultado')
+def download_resultado():
+    return send_file('./data/resultado.csv', as_attachment=True)
 
 if __name__ == '__main__':
    app.run(debug=True)
